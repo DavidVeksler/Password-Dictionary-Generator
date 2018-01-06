f6e822bcd94385a6c13630378b382a77eb050b35
David Veksler <heroic@gmail.com>
2018-01-06 17:35:58 -0500

    Dictionary generator

---
+++ PasswordDictionaryGenerator/DictionaryGenerator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PasswordDictionaryGenerator
+{
+    public class DictionaryGenerator
+    {
+        public string GeneratePassword(
+            string validChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ", 
+            string prefix = "", 
+            int maxlength = 12,
+            string fileName = null
+            )
+        {
+            value = new StringBuilder();
+            _maxlength = maxlength;
+            ValidChars = validChars;
+
+            Dive(prefix, 0);
+
+            if (!string.IsNullOrWhiteSpace(fileName))
+            {
+                System.IO.File.WriteAllText(fileName,value.ToString());
+                return value.Length.ToString() + " chars";
+            }
+            else
+            {
+                return value.ToString();
+            }
+
+            
+        }
+
+        private StringBuilder value = new StringBuilder();
+        int _maxlength = 12;
+        string ValidChars;
+
+
+        private void Dive(string prefix, int level)
+        {
+            level += 1;
+            foreach (char c in ValidChars)
+            {
+                Console.WriteLine(prefix + c);
+                value.AppendLine(prefix + c);
+
+                if (level < _maxlength)
+                {
+                    Dive(prefix + c, level);
+                }
+            }
+        }
+
+    }
+}
--- PasswordDictionaryGenerator/Program.cs
+++ PasswordDictionaryGenerator/Program.cs
@@ -7,6 +7,15 @@ namespace PasswordDictionaryGenerator
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var list = new DictionaryGenerator().GeneratePassword(maxlength:3);
+
+            Console.Write(list);
+
+            Console.ReadKey();
+
+
+
         }
     }
 }
