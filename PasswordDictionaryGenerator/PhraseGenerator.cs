addcf790b13e682d173be64944253688aa94c13f
David Veksler <heroic@gmail.com>
2018-01-06 18:10:35 -0500

    add PhraseGenerator

---
+++ PasswordDictionaryGenerator/PhraseGenerator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PasswordDictionaryGenerator
+{
+    public class PhraseGenerator
+    {
+        private StringBuilder value = new StringBuilder();
+        private int _minWords = 0;
+        private int _maxWords = 12;
+
+        List<string> _wordList;
+
+        public string GeneratePhrase(List<string> wordList, int minWords = 0, int maxWords = 12, string fileName = null, string dictionaryFile = null)
+        {
+            value = new StringBuilder();
+            _minWords = minWords;
+            _maxWords = maxWords;
+            _wordList = wordList;
+
+            if (!string.IsNullOrWhiteSpace(dictionaryFile))
+            {
+                _wordList = System.IO.File.ReadAllLines(dictionaryFile).ToList();
+            }
+
+
+            Dive("", minWords);
+
+            if (!string.IsNullOrWhiteSpace(fileName))
+            {
+                System.IO.File.WriteAllText(fileName, value.ToString());
+                return value.Length.ToString() + " chars";
+            }
+            else
+            {
+                return value.ToString();
+            }
+        }
+
+        private void Dive(string prefix, int level)
+        {
+            level += 1;
+            foreach (string word in _wordList)
+            {
+                Console.WriteLine(prefix + " " + word);
+                value.AppendLine(prefix + " " + word);
+
+                if (level < _maxWords)
+                {
+                    Dive(prefix + " " + word, level);
+                }
+            }
+        }
+
+    }
+}
--- PasswordDictionaryGenerator/Program.cs
+++ PasswordDictionaryGenerator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PasswordDictionaryGenerator
 {
@@ -8,9 +9,16 @@ namespace PasswordDictionaryGenerator
         {
             Console.WriteLine("Hello World!");
 
-            var list = new DictionaryGenerator().GeneratePassword(maxlength:3);
+            //var list = new DictionaryGenerator().GeneratePassword(maxlength:3);
 
-            Console.Write(list);
+            var wordList = new List<string>();
+
+            wordList.Add("apple");
+            wordList.Add("banana");
+            wordList.Add("rasberry");
+            var list = new PhraseGenerator().GeneratePhrase(wordList: wordList, maxWords:2, dictionaryFile: "dictionary.txt");
+
+           // Console.Write(list);
 
             Console.ReadKey();
 
